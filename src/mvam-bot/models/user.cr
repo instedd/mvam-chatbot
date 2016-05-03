fc03965fde36564529e38e358edba8c5ee150c2c
Santiago Palladino <spalladino@gmail.com>
2016-05-03 20:30:11 -0300

    User model

---
+++ src/mvam-bot/models/user.cr
@@ -0,0 +1,14 @@
+module MvamBot
+  class User
+
+    id : Int32?
+    username : String?
+    name : String?
+    location_adm0_id : Int32?
+    location_adm1_id : Int32?
+    location_mkt_id : Int32?
+    location_lat : Float?
+    location_lng : Float?
+
+  end
+end
